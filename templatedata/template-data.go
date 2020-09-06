ab3afc3c9af068b05a888eb493c32a6e0201f0bf
Trevor Sawler <trevor.sawler@gmail.com>
2020-09-06 11:43:28 -0300

    Formatting

--- templatedata/template-data.go
+++ templatedata/template-data.go
@@ -15,6 +15,6 @@ func NewTemplateData(p *sql.DB) {
 
 // AddDefaultData adds default data for templates
 func AddDefaultData(td *templates.TemplateData) *templates.TemplateData {
-	td.ClientVersion = "Courses v1.0.0"
+	td.ClientVersion = `<strong><a href="https://github.com/tsawler/courses/" target="_blank">Courses</a></strong> v1.0.0`
 	return td
 }
