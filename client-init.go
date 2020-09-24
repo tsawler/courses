347a437fa39c2e7604d8a0a4b6511f8887237a6e
Trevor Sawler <trevor.sawler@gmail.com>
2020-09-24 13:39:38 -0300

    Client init improvements

--- client-init.go
+++ client-init.go
@@ -16,6 +16,8 @@ var parentDB *driver.DB
 var repo *handlers.DBRepo
 var dbModel *clientdb.DBModel
 
+const version = "1.3.2"
+
 // ClientInit  initializes client specific code
 func ClientInit(conf config.AppConfig, parentDriver *driver.DB, rep *handlers.DBRepo) {
 	// conf is the application config, from goBlender
@@ -31,7 +33,7 @@ func ClientInit(conf config.AppConfig, parentDriver *driver.DB, rep *handlers.DB
 
 	// enable db for client access
 	dbModel = &clientdb.DBModel{DB: parentDB.SQL}
-	template_data.NewTemplateData(parentDB.SQL)
+	template_data.NewTemplateData(parentDB.SQL, version)
 
 	// We can access handlers from goBlender, but need to initialize them first.
 	if app.Database == "postgresql" {
@@ -39,6 +41,9 @@ func ClientInit(conf config.AppConfig, parentDriver *driver.DB, rep *handlers.DB
 	} else {
 		handlers.NewMysqlHandlers(parentDB, app.ServerName, app.InProduction)
 	}
+
+	infoLog.Printf("******************************************")
+	infoLog.Printf("** %sCourses%s v%s", "\033[31m", "\033[0m", version)
 
 	// Create client middleware
 	NewClientMiddleware(app)
--- templatedata/template-data.go
+++ templatedata/template-data.go
@@ -2,19 +2,22 @@ package templatedata
 
 import (
 	"database/sql"
+	"fmt"
 	"github.com/tsawler/goblender/client/clienthandlers/clientdb"
 	"github.com/tsawler/goblender/pkg/templates"
 )
 
 var dbModel *clientdb.DBModel
+var version string
 
 // NewTemplateData sets database connection
-func NewTemplateData(p *sql.DB) {
+func NewTemplateData(p *sql.DB, v string) {
 	dbModel = &clientdb.DBModel{DB: p}
+	version = v
 }
 
 // AddDefaultData adds default data for templates
 func AddDefaultData(td *templates.TemplateData) *templates.TemplateData {
-	td.ClientVersion = `<strong><a href="https://github.com/tsawler/courses/" target="_blank">Courses</a></strong> v1.3.1`
+	td.ClientVersion = fmt.Sprintf(`<strong><a href="https://github.com/tsawler/courses/" target="_blank">Courses</a></strong> %s`, version)
 	return td
 }
