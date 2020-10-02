c9d1ca1094dd8bf8bdc7eb8727ee87658f413de9
Trevor Sawler <trevor.sawler@gmail.com>
2020-10-02 13:47:18 -0300

    Add role

---
+++ migrations/20201002164445_add_courses_role.postgres.down.sql
@@ -0,0 +1,1 @@
+delete from roles where role_name = 'courses';
---
+++ migrations/20201002164445_add_courses_role.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO "public"."roles"("role_name","role","active","created_at","updated_at")
+VALUES
+(E'Manage Courses',E'courses',1,E'2020-10-02 00:00:00',E'2020-10-02 00:00:00');
